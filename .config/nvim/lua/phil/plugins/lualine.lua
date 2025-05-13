0889276bab56f1e90fa61bed49f9a0a67591fc62
Philip Rader <pjrader1@yahoo.com>
2025-05-12 18:15:16 -0700

    Attempt to add lualine
    Haven't figured out how to get it to work with wintabs

---
+++ .config/nvim/lua/phil/plugins/lualine.lua
@@ -0,0 +1,53 @@
+-- I can't get this working with wintabs right now
+return {
+    "nvim-lualine/lualine.nvim",
+    enabled = falses,
+    lazy = false,
+    cond = not vim.g.vscode,
+    priority = 100,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "nvim-lua/lsp-status.nvim",
+    },
+    config = function()
+        require('lualine').setup {
+            options = {
+                icons_enabled = true,
+                theme = 'auto',
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                },
+                ignore_focus = {},
+                always_divide_middle = true,
+                always_show_tabline = true,
+                globalstatus = false,
+                refresh = {
+                    statusline = 0,
+                    tabline = 0,
+                    winbar = 0,
+                }
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            extensions = {}
+        }
+    end
+}
