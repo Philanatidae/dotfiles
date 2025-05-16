a624a11f8aeea4881e75f10975b4701f732bdd64
Philip Rader <pjrader1@yahoo.com>
2025-05-16 15:54:13 -0600

    Add objc/objcpp file types

--- .config/nvim/lua/phil/filetypes.lua
+++ .config/nvim/lua/phil/filetypes.lua
@@ -1,6 +1,8 @@
 vim.cmd([[
     autocmd BufRead,BufNewFile *.hm set filetype=objc
+    autocmd BufRead,BufNewFile *.m set filetype=objc
     autocmd BufRead,BufNewFile *.hmm set filetype=objcpp
+    autocmd BufRead,BufNewFile *.mm set filetype=objcpp
 
     autocmd BufRead,BufNewFile *.vert set filetype=glsl
     autocmd BufRead,BufNewFile *.frag set filetype=glsl
