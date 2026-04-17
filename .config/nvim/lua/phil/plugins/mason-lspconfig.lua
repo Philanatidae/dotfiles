f9721da626728ec4d7daf384f8eef5b19ea968e3
Philip Rader <pjrader1@yahoo.com>
2026-04-17 17:14:18 -0600

    Remove ensure_installed for LSP servers

--- .config/nvim/lua/phil/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/phil/plugins/mason-lspconfig.lua
@@ -7,23 +7,6 @@ return {
     enabled = true,
     lazy = false,
     cond = not vim.g.vscode,
-    config = function()
-        -- @todo I might not even need this anymore, can't I just manually install LSPs that I need?
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                -- 'asm-lsp', Incorrect name, need to rethink this
-                'clangd',
-                'lua_ls',
-                'cmake',
-                'html',
-                'pylsp',
-                'jsonls',
-                'cssls',
-                'eslint',
-                'ts_ls',
-                'gopls',
-            },
-            automatic_enable = true,
-       })
-    end
+    opts = {
+    },
 }
