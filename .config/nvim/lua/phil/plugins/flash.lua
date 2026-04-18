a2e74ce74b6e18ca4065846901d67418ef9de90b
Philip Rader <pjrader1@yahoo.com>
2026-04-18 13:29:49 -0600

    Add flash.nvim

---
+++ .config/nvim/lua/phil/plugins/flash.lua
@@ -0,0 +1,16 @@
+return {
+    'folke/flash.nvim',
+    -- There's some issue with the highlighting group in my theme I think
+    enabled = false,
+    lazy = true,
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+        { 's', mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        -- { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+        -- { "f", mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
+        -- { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        -- { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
+}
--- .config/nvim/lua/phil/plugins/which-key.lua
+++ .config/nvim/lua/phil/plugins/which-key.lua
@@ -4,9 +4,7 @@ return {
     lazy = true,
     event = 'VeryLazy',
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        preset = 'classic',
     },
     keys = {
         {
