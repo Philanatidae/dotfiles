d4114039d67563d5cc51ca41d8ce4b7a0af27e72
Philip Rader <pjrader1@yahoo.com>
2026-04-18 12:14:20 -0600

    Add descriptions to remap.lua

--- .config/nvim/lua/phil/remap.lua
+++ .config/nvim/lua/phil/remap.lua
@@ -39,41 +39,43 @@ if (vim.g.vscode) then
     vim.keymap.set("n", "<leader>j", ":<C-u>exe ':wincmd ' . v:count1 . 'j'<CR>")
     vim.keymap.set("n", "<leader>h", ":<C-u>exe ':wincmd ' . v:count1 . 'h'<CR>")
 else
-    vim.keymap.set("n", "<leader>w", "<cmd>update<CR>")
-    vim.keymap.set("n", "<leader>W", "<cmd>bufdo update<CR>")
+    vim.keymap.set("n", "<leader>w", "<cmd>update<CR>", { desc = "Write buffer" })
+    vim.keymap.set("n", "<leader>W", "<cmd>bufdo update<CR>", { desc = "Write all buffers" })
     vim.keymap.set("n", "<leader><C-q>", function()
         vim.cmd("qa")
-    end)
+    end, { desc = "Quit Neovim" })
 
     -- Window management
-    vim.keymap.set("n", "=", ":<C-u>exe 'vertical resize +' . v:count1<CR>")
-    vim.keymap.set("n", "-", ":<C-u>exe 'vertical resize -' . v:count1<CR>")
-    vim.keymap.set("n", "+", ":<C-u>exe 'resize +' . v:count1<CR>")
-    vim.keymap.set("n", "_", ":<C-u>exe 'resize -' . v:count1<CR>")
+    vim.keymap.set("n", "=", ":<C-u>exe 'vertical resize +' . v:count1<CR>", { desc = "Increase window height" })
+    vim.keymap.set("n", "-", ":<C-u>exe 'vertical resize -' . v:count1<CR>", { desc = "Decrease window height" })
+    vim.keymap.set("n", "+", ":<C-u>exe 'resize +' . v:count1<CR>", { desc = "Increase window width" })
+    vim.keymap.set("n", "_", ":<C-u>exe 'resize -' . v:count1<CR>", { desc = "Decrease window width" })
 
-    vim.keymap.set("n", "<leader>l", ":<C-u>exe ':wincmd ' . v:count1 . 'l'<CR>")
-    vim.keymap.set("n", "<leader>k", ":<C-u>exe ':wincmd ' . v:count1 . 'k'<CR>")
-    vim.keymap.set("n", "<leader>j", ":<C-u>exe ':wincmd ' . v:count1 . 'j'<CR>")
-    vim.keymap.set("n", "<leader>h", ":<C-u>exe ':wincmd ' . v:count1 . 'h'<CR>")
+    vim.keymap.set("n", "<leader>l", ":<C-u>exe ':wincmd ' . v:count1 . 'l'<CR>",
+        { desc = "Move cursor to right window" })
+    vim.keymap.set("n", "<leader>k", ":<C-u>exe ':wincmd ' . v:count1 . 'k'<CR>",
+        { desc = "Move cursor to above window" })
+    vim.keymap.set("n", "<leader>j", ":<C-u>exe ':wincmd ' . v:count1 . 'j'<CR>",
+        { desc = "Move cursor to below window" })
+    vim.keymap.set("n", "<leader>h", ":<C-u>exe ':wincmd ' . v:count1 . 'h'<CR>", { desc = "Move cursor to left window" })
 
     -- Diagnostics
-    vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end)
+    vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, { desc = "Open diagnostic window" })
 
     vim.keymap.set('i', '<C-c>', '<Nop>')
     vim.keymap.set('v', '<C-c>', '<Nop>')
 
-    -- Move lines up and down in VISUAL mode
-    vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-    vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+    vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+    vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 
-    vim.keymap.set("n", "J", "mzJ`z")
+    vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines at cursor" })
 
-    vim.keymap.set("n", "<C-d>", "<C-d>zz")
-    vim.keymap.set("n", "<C-u>", "<C-u>zz")
-    vim.keymap.set("n", "n", "nzzzv")
-    vim.keymap.set("n", "N", "Nzzzv")
+    vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" })
+    vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
+    vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result" })
+    vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result" })
 end
 
 vim.keymap.set("n", "U", function()
     vim.cmd("redo")
-end)
+end, { desc = "Redo", })
