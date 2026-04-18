265deb7cff4f27557ea52e8bdabe034550eaf98e
Philip Rader <pjrader1@yahoo.com>
2026-04-18 12:14:14 -0600

    Add additional configuration

--- .config/nvim/lua/phil/set.lua
+++ .config/nvim/lua/phil/set.lua
@@ -1,8 +1,12 @@
+-- General
 vim.opt.backup = false      -- No backup before overwriting file
 vim.opt.writebackup = false -- ^^^
-vim.opt.updatetime = 750    -- 750ms after no input before writing swap file
-vim.opt.confirm = true      -- Prompt to save buffer if unsaved
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.confirm = true
 vim.opt.exrc = true
+vim.opt.undofile = true
+vim.opt.mouse = "a"
 
 -- Indent w/ 4 spaces
 vim.opt.tabstop = 4
@@ -10,9 +14,39 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
+vim.opt.breakindent = true
 
--- Enable mouse
-vim.opt.mouse = "a"
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.inccommand = "split"
+
+-- Appearance
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Auto switch between relative and normal
+local numbertoggle_augroup = vim.api.nvim_create_augroup("numbertoggle", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
+    pattern = "*",
+    group = numbertoggle_augroup,
+    command = "if &nu && mode() != 'i' | set rnu   | endif",
+})
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
+    pattern = "*",
+    group = numbertoggle_augroup,
+    command = "if &nu | set nornu | endif",
+})
 
 -- If using wl-clipboard, we need to set manually so that *
 -- is the same as +.
@@ -33,28 +67,6 @@ if vim.env.WAYLAND_DISPLAY ~= nil and vim.env.WAYLAND_DISPLAY ~= ''
     }
 end
 
-vim.opt.cursorline = true  -- Highlight line cursor is on
-vim.opt.signcolumn = "yes" -- Column on left for breakpoints, etc.
-vim.opt.number = true      -- Enable line numbers
-
--- Auto switch between relative and normal
-local numbertoggle_augroup = vim.api.nvim_create_augroup("numbertoggle", { clear = true })
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
-    pattern = "*",
-    group = numbertoggle_augroup,
-    command = "if &nu && mode() != 'i' | set rnu   | endif",
-})
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
-    pattern = "*",
-    group = numbertoggle_augroup,
-    command = "if &nu | set nornu | endif",
-})
-
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
 
 local sev = vim.diagnostic.severity
 vim.diagnostic.config({
