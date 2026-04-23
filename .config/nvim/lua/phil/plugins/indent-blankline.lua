cfd89616cc24de798707aadd2e0f2a5993cf0b6f
Philip Rader <pjrader1@yahoo.com>
2026-04-23 11:42:34 -0600

    Add indent-blankline.nvim

---
+++ .config/nvim/lua/phil/plugins/indent-blankline.lua
@@ -0,0 +1,13 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    enabled = true,
+    lazy = false,
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+        scope = {
+            enabled = false,
+        },
+    },
+}
