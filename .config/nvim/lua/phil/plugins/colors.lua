5c40d8349374ea46c53f0304668bc7f38fa68eee
Philip Rader <pjrader1@yahoo.com>
2026-04-23 11:42:17 -0600

    Experiment with tokyonight

--- .config/nvim/lua/phil/plugins/colors.lua
+++ .config/nvim/lua/phil/plugins/colors.lua
@@ -1,13 +1,34 @@
-return {
-    'Philanatidae/night-owl.nvim',
-    enabled = true,
-    lazy = false,
-    cond = not vim.g.vscode,
-    priority = 1000,
-    config = function()
-        -- @todo Add light theme
-        -- @todo Remove this hard-coded color change (lightline.lua also has a hard-coded value)
-        require('night-owl').setup()
-        vim.cmd('colorscheme night-owl')
-    end
+local ColorSchemes = {
+    NIGHT_OWL = 1,
+    TOKYO_NIGHT = 2,
 }
+
+local colorscheme = ColorSchemes.TOKYO_NIGHT
+
+if (colorscheme == ColorSchemes.NIGHT_OWL) then
+    return {
+        'Philanatidae/night-owl.nvim',
+        enabled = true,
+        lazy = false,
+        cond = not vim.g.vscode,
+        priority = 1000,
+        config = function()
+            -- @todo Add light theme
+            require('night-owl').setup()
+            vim.cmd('colorscheme night-owl')
+        end
+    }
+end
+if (colorscheme == ColorSchemes.TOKYO_NIGHT) then
+    return {
+        'folke/tokyonight.nvim',
+        enabled = true,
+        lazy = false,
+        cond = not vim.g.vscode,
+        priority = 1000,
+        config = function()
+            require('tokyonight').setup()
+            vim.cmd('colorscheme tokyonight')
+        end
+    }
+end
