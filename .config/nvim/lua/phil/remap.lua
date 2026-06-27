dcfafb61f5c520184afb423c3ab60b462a974e6a
Philip Rader <pjrader1@yahoo.com>
2026-06-27 08:13:55 -0600

    Clean up remap.lua

--- .config/nvim/lua/phil/remap.lua
+++ .config/nvim/lua/phil/remap.lua
@@ -1,95 +1,42 @@
 vim.g.mapleader = ' '
 
-if (vim.g.vscode) then
-    local vscode = function(cmd)
-        require('vscode-neovim').call(cmd)
-    end
+-- Disable middle-click paste
+vim.keymap.set({ 'n', 'i', 'v' }, '<MiddleMouse>', '<Nop>')
+vim.keymap.set({ 'n', 'i', 'v' }, '<2-MiddleMouse>', '<Nop>')
+vim.keymap.set({ 'n', 'i', 'v' }, '<3-MiddleMouse>', '<Nop>')
+vim.keymap.set({ 'n', 'i', 'v' }, '<4-MiddleMouse>', '<Nop>')
 
-    vim.keymap.set('n', '<leader>w', function()
-        vscode('workbench.action.files.save')
-    end)
-    vim.keymap.set('n', '<leader>W', function()
-        vscode('workbench.action.files.saveAll')
-    end)
-    vim.keymap.set('n', '<leader><C-q>', function()
-        vscode('workbench.action.closeAllEditors')
-    end)
+vim.keymap.set('n', '<leader>w', '<cmd>update<CR>', { desc = "Write buffer" })
+vim.keymap.set('n', '<leader>W', '<cmd>bufdo update<CR>', { desc = "Write all buffers" })
+vim.keymap.set('n', '<leader><C-q>', '<cmd>qa<CR>', { desc = 'Quit Neovim' })
+-- These two will be overridden by vim-wintabs if it's enabled
+vim.keymap.set('n', '<leader>q', '<cmd>bd<CR>', { desc = 'Close current buffer' })
+vim.keymap.set('n', '<leader>Q', '<cmd>q<CR>', { desc = 'Close current window' })
 
-    -- These don't work correctly (specifically, =, which
-    -- has something else bound do it =?, I've got no idea
-    -- what). This works in neovim proper, but not in vscode.
-    -- Oh well, screw that for now.
-    --
-    -- Also need to add count arg
-    -- vim.keymap.set('n', '=', function()
-    --     vscode('workbench.action.increaseViewWidth')
-    -- end)
-    -- vim.keymap.set('n', '-', function()
-    --     vscode('workbench.action.decreaseViewWidth')
-    -- end)
-    -- vim.keymap.set('n', '+', function()
-    --     vscode('workbench.action.increaseViewHeight')
-    -- end)
-    -- vim.keymap.set('n', '_', function()
-    --     vscode('workbench.action.decreaseViewHeight')
-    -- end)
-
-    vim.keymap.set('n', '<leader>l', ":<C-u>exe ':wincmd ' . v:count1 . 'l'<CR>")
-    vim.keymap.set('n', '<leader>k', ":<C-u>exe ':wincmd ' . v:count1 . 'k'<CR>")
-    vim.keymap.set('n', '<leader>j', ":<C-u>exe ':wincmd ' . v:count1 . 'j'<CR>")
-    vim.keymap.set('n', '<leader>h', ":<C-u>exe ':wincmd ' . v:count1 . 'h'<CR>")
-else
-    -- Disable middle-click paste
-    vim.keymap.set({ 'n', 'i', 'v' }, '<MiddleMouse>', '<Nop>')
-    vim.keymap.set({ 'n', 'i', 'v' }, '<2-MiddleMouse>', '<Nop>')
-    vim.keymap.set({ 'n', 'i', 'v' }, '<3-MiddleMouse>', '<Nop>')
-    vim.keymap.set({ 'n', 'i', 'v' }, '<4-MiddleMouse>', '<Nop>')
-
-    vim.keymap.set('n', '<leader>w', '<cmd>update<CR>', { desc = "Write buffer" })
-    vim.keymap.set('n', '<leader>W', '<cmd>bufdo update<CR>', { desc = "Write all buffers" })
-    vim.keymap.set('n', '<leader><C-q>', function()
-        vim.cmd('qa')
-    end, { desc = 'Quit Neovim' })
-
-    -- Window management
-    vim.keymap.set('n', '<leader>l', ":<C-u>exe ':wincmd ' . v:count1 . 'l'<CR>",
+-- Window management
+for _, key in ipairs({ 'h', 'j', 'k', 'l' }) do
+    vim.keymap.set('n', '<leader>' .. key,
+        (":<C-u>exe ':wincmd ' . v:count1 . '%s'<CR>"):format(key),
         {
             silent = true,
-            desc = 'Move cursor to right window',
+            desc = ('Go to the %s window'):format(key),
         })
-    vim.keymap.set('n', '<leader>k', ":<C-u>exe ':wincmd ' . v:count1 . 'k'<CR>",
-        {
-            silent = true,
-            desc = 'Move cursor to above window',
-        })
-    vim.keymap.set('n', '<leader>j', ":<C-u>exe ':wincmd ' . v:count1 . 'j'<CR>",
-        {
-            silent = true,
-            desc = 'Move cursor to below window',
-        })
-    vim.keymap.set('n', '<leader>h', ":<C-u>exe ':wincmd ' . v:count1 . 'h'<CR>",
-        {
-            silent = true,
-            desc = 'Move cursor to left window',
-        })
-
-    -- Diagnostics
-    vim.keymap.set('n', '<leader>e', function() vim.diagnostic.open_float() end, { desc = 'Open diagnostic window' })
-
-    vim.keymap.set('i', '<C-c>', '<Nop>')
-    vim.keymap.set('v', '<C-c>', '<Nop>')
-
-    vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
-    vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
-
-    vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join lines at cursor' })
-
-    vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down' })
-    vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up' })
-    vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result' })
-    vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous search result' })
 end
 
-vim.keymap.set('n', 'U', function()
-    vim.cmd('redo')
-end, { desc = 'Redo', })
+-- Diagnostics
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open diagnostic window' })
+
+-- @todo Do I need these anymore?
+-- vim.keymap.set('i', '<C-c>', '<Nop>')
+-- vim.keymap.set('v', '<C-c>', '<Nop>')
+-- vim.keymap.set('n', 'U', '<C-r>', { desc = 'Redo', })
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
+
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join lines at cursor' })
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up' })
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous search result' })
